af9fa6b02508086751e9f09d1b3e95528f7d5319
Acha-Goodness <acharious1668@gmail.com>
2025-08-16 09:01:18 -0700

    created chat sidebar

--- src/components/home-view/sidebar.jsx
+++ src/components/home-view/sidebar.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { getUser } from '@/store/chat-slice';
+import { useDispatch, useSelector } from 'react-redux';
+import SidebarSkeleton from './sidebarskeleton';
 
 const SideBar = () => {
+  const { users, selectedUser, isUsersLoading } = useSelector( state => state.chat);
+  const dispatch = useDispatch();
+  
+  const onlineUsers = [];
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
+
+  if(isUsersLoading) return <SidebarSkeleton/>
+
   return (
-    <div>sidebar</div>
+    <aside className='h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200'>
+
+    </aside>
   )
 }
 
