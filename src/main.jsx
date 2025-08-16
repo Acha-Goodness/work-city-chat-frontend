6c6f5fb5674c7d69b21198f7562daf8c23809957
Acha-Goodness <acharious1668@gmail.com>
2025-08-16 02:54:38 -0700

    integrated redux store in mainJS file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { Toaster } from "@/components/ui/sonner"
 import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 createRoot(document.getElementById('root')).render(
-      <App /> 
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+      <Toaster/>
+    </Provider>
+  </BrowserRouter>,
 )
